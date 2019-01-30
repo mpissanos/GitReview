683c2e25aa8318bda84371fdb6dbb1b0019dbd10
mpissanos <mpissano@gmail.com>
2019-01-30 12:40:12 -0500

    devise users

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,21 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!
+
+
   def index
     @reviews = Review.all
   end
+
+  def new
+    @review = Review.new
+    @review.user = current_user
+    @review.guitar = Guitar.find(params:[:guitar_id])
+  end
+
+  private
+
+  def review_params
+      params.require(:review).permit(:review_content, :group_id, :id)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   
   resources :reviews, only: [:index, :new, :create]
   resources :guitars
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: { registrations: 'registrations', omniauth_callbacks: 'callbacks' }
   
 end
